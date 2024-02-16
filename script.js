1e594b306b3ca01aabde23ce9baed06c3355cd79
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-16 19:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function mapLetters(str){
+    let obj = {};
+    for(let i = 0; i < str.length; i++){
+        let char = str.charAt(i);
+        if(obj[char]){
+            obj[char].push(i);
+        } else {
+            obj[char] = [i];
+        }
+    }
+    return obj;
+}
